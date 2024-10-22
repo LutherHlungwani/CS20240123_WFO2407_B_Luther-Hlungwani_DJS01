ee5748500b75ffe90686f34a6e44b0d99b67247d
Luther Hlungwani <lhhlungwani3@gmail.com>
2024-10-22 14:42:59 +0200

    error statement for fuel missing

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
+const distance_CONVERSION_RATE = 1000;
+
+
 // Given Parameters
 const vel = 10000; // velocity (km/h)
 const acc = 3; // acceleration (m/s^2)
@@ -13,15 +16,33 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
+const velocityInMeterSec = (vel* 1000) / 3600;
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const calcNewVel = ({vel, acc, time}) => {
+
+  if (vel < 0) {
+    throw Error ('velocity cannot be negative') ;
+  }
+  if (time < 0) {
+    throw Error ('Time cannot be negative');
+  }
+
+  if (acc < 0) {
+    throw Error ('Accelaration cannot be negative');
+  }
+
+  const newVelocityInMeterSec = vel + (acc * time);
+  return (newVelocityInMeterSec * 3600) / 1000;
+}
+
+
+
+
+const d2 = d + (velocityInMeterSec*time)/1000; //calcultes new distance
+const rf = fuel - fbr*time; //calculates remaining fuel
+const vel2 = calcNewVel({vel: velocityInMeterSec, acc, time}); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-}
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
